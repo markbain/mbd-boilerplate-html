117fe8e8ebac0c05048ae3ad530b374bd25aba50
Mark Bain <markbaindesign@gmail.com>
2015-09-14 18:41:55 +0200

    remove no-js class

--- httpdocs/assets/js/source/custom/custom.js
+++ httpdocs/assets/js/source/custom/custom.js
@@ -1,5 +1,29 @@
-$(function() {
+/*
+	===============================================
+
+		Remove "no-js" class  
+
+	===============================================
+*/	
+
+$(document).ready(function( $ ){
+	document.documentElement.className = 
+	document.documentElement.className.replace("no-js","js");
+});
+
+/*
+	===============================================
+
+		Match height  
+
+	===============================================
+*/
+
+$(document).ready(function( $ ){
     $('.media-object').matchHeight({
+    	byRow: false
+	});
+    $('.branding-object').matchHeight({
     	byRow: false
 	});
 });
